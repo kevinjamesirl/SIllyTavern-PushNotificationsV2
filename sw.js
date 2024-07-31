d6cbe1a6718eee4c12bcc4b9067481112438c6c1
LilTables <scr3wgr4vity@gmail.com>
2024-07-31 15:11:32 -0700

    change trigger

--- sw.js
+++ sw.js
@@ -3,7 +3,14 @@ console.log('Hello from service worker!');
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
     event.waitUntil(
-        clients.matchAll({ type: 'window' }).then((clientList) => {
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+            // Check if there is already a window/tab open with this site
+            for (const client of clientList) {
+                if (client.url.includes('/') && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If no window/tab is open, open a new one
             if (clients.openWindow) {
                 return clients.openWindow('/');
             }
